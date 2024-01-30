032961be6b2f98ae5391e016d65c86cb2bf90f2c
evan <esondgeroth13@gmail.com>
2024-01-30 16:56:02 -0600

    Begin to Display Current Song t itle and artist in player display

--- script.js
+++ script.js
@@ -130,6 +130,11 @@ const playPreviousSong = () => {
    }
 };
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
