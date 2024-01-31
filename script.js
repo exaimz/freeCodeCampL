dbd5072f74c7871e72a24db026211edb24388f1f
exaimz <65930249+exaimz@users.noreply.github.com>
2024-01-30 19:03:24 -0600

    Create the shuffle function (#2)
    * Create arrow function to set acc text
    
    * setAttribute(aria-label) for an accessible play
    
    * add new func to PlaySong func
    
    * Create new arrow function for the Shuffle feature
    
    * Use sort() on userData.songs array with a callback function that randomizes the songs
    
    * empty the currentSong & currentSongTime properties
    
    * Re-render songs & call up functions
    
    * Add event listener for the shuffle button

--- script.js
+++ script.js
@@ -132,6 +132,16 @@ const playPreviousSong = () => {
    }
 };
 
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
@@ -196,6 +206,8 @@ pauseButton.addEventListener("click",  pauseSong);
 nextButton.addEventListener("click", playNextSong);
 
 previousButton.addEventListener("click", playPreviousSong);
+
+shuffleButton.addEventListener("click", shuffle);
 
 userData?.songs.sort((a,b) => {
   if (a.title < b.title) {
