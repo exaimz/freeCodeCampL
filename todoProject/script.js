0394f48421d99181d4911c7eb5dc9b91998f0a79
evan <esondgeroth13@gmail.com>
2024-02-03 05:04:37 -0600

    Catch up

--- todoProject/script.js
+++ todoProject/script.js
@@ -17,11 +17,61 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+//Create reset function (for form fields)
+const reset = () => {
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+    taskForm.classList.toggle("hidden");
+    currentTask = {};
+};
+
 //Open and close the task form
-openTaskFormBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
-closeTaskFormBtn.addEventListener("click", () => confirmCloseDialog.showModal());
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  };
+}
+);
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
-  });
+  confirmCloseDialog.close();
+  reset();
+});
+
+//Get values from form input fields
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+  //Save task into taskData array
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  //Display values on the page
+  taskData.forEach(
+    ({ id, title, date, description }) =>
+      (tasksContainer.innerHTML += `
+    <div class="task" id="${id}"></div>
+    <p><strong>Title: </strong>${title}</p>
+    <p><strong>Date: </strong>${date}</p>
+    <p><strong>Description: </strong>${description}</p>
+    <button type="button" class="btn">Edit</button>
+    <button type="button" class="btn">Delete</button>
+  `)
+  );
+  //close form to view tasks
+  reset();
+});
