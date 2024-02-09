4984dd1fdd5d92a8f757192dac759f0d7bf89e70
evan <esondgeroth13@gmail.com>
2024-02-09 02:14:14 -0600

    create binary function

--- binaryConverter/script.js
+++ binaryConverter/script.js
@@ -2,7 +2,28 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => {};
+const decimalToBinary = (input) => {
+  //create some arrays to store the inputs and results of the division
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+
+};
 
 const checkUserInput = () => {
   //Create an alert that tells the user they dont have a number or it is invalid
