d3bf082e48652d3a5ddc2e6deb929af5f32ed02b
evan <esondgeroth13@gmail.com>
2024-02-21 12:28:07 -0600

    Add roman numeral object

--- romanNumeralProject/script.js
+++ romanNumeralProject/script.js
@@ -1,6 +1,21 @@
 const number = document.getElementById("number");
 const button = document.getElementById("convert-btn");
 const output = document.getElementById("output");
+const romanNumeralObj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1, 
+};
 
 const checkUserInput = () => {
     if (!number.value) {
@@ -8,6 +23,8 @@ const checkUserInput = () => {
         return;
     } else if (number.value <= -1) {
         output.textContent = "Please enter a number greater than or equal to 1";
+    } else if (number.value >= 4000) {
+        output.textContent = " Please enter a number less than or equal to 3999";
     }
 }
 
