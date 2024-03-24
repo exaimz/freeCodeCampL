91789a6a2198e0766a51efc764d8591060d7db22
evan <esondgeroth13@gmail.com>
2024-03-24 16:16:03 -0500

    Create fade in animation for tasks and refactor code

--- todoApp/personalToDo/app.js
+++ todoApp/personalToDo/app.js
@@ -5,50 +5,40 @@ const resultDiv = document.getElementById("result-div");
 const taskList = document.getElementById("task-list");
 
 const newTask = (input) => {
+    //CREATE TASK ELEMENT
     const newTaskDiv = document.createElement("div");
+    newTaskDiv.classList.add("newTask");
+    newTaskDiv.classList.add("fade-in");
+
+    //CREATE TASK TEXT
     const taskText = document.createElement("p");
+    taskText.classList.add("taskText");
+    taskText.innerHTML = input;
+
+    //CREATE COMPLETED BUTTON
     const completedButton = document.createElement("button");
+    completedButton.classList.add("completedButton");
+    completedButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+
+    //CREATE DELETE BUTTON
     const deleteButton = document.createElement("button");
-    
+    deleteButton.classList.add("deleteButton");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
 
+    //APPEND ELEMENTS 
+    resultDiv.appendChild(newTaskDiv);
+    newTaskDiv.appendChild(completedButton);
+    newTaskDiv.appendChild(taskText);
+    newTaskDiv.appendChild(deleteButton);
 }
-
 
 const newTaskEvent = form.addEventListener("submit", (e) => {
     //Prevent page reloading on submit
     e.preventDefault();
-    //Create each task their own div
-    const newTaskDiv = document.createElement("div");
 
-    //Create Completed button
-    const completedButton = document.createElement("button");
-    completedButton.classList.add("completedButton");
+    //Create New Task
+    newTask(taskInput.value);
 
-    //Add Contented to complete button
-    completedButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
-
-    //Append to task div
-    newTaskDiv.appendChild(completedButton);
-
-    //Get task value from input and append it to the new li element
-    const newTaskText = taskInput.value;
-    const liElement = document.createElement("p");
-
-    resultDiv.appendChild(newTaskDiv);
-    newTaskDiv.classList.add("newTask");
-
-    newTaskDiv.appendChild(liElement);
-    liElement.classList.add("taskText");
-
-
-    liElement.innerHTML = newTaskText;
-
-    //create delete and complete buttons for new li element
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("deleteButton");
-
-    newTaskDiv.appendChild(deleteButton);
-    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-
+    //Clear input
     taskInput.value = "";  
 });
